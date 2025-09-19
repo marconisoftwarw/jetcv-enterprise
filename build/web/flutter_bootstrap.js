aa1c8150ff66cbf942cc1e726ce123899c719b29
Riccardo Pirani <riccardo.pirani@reiser.it>
2025-09-19 07:41:55 +0200

    Fix code

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3269423816"
+    serviceWorkerVersion: "2734165176"
   }
 });
