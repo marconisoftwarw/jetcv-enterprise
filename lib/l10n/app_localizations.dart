bff058a907285f8de763802233b1f439ea11691f
Riccardo Pirani <riccardo.pirani@reiser.it>
2025-09-24 16:42:01 +0200

    fix key i10N

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 // Supporto per le lingue
