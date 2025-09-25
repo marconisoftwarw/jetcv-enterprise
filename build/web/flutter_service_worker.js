0a56763d44e72171c62c840290274bcdd15ff427
Riccardo Pirani <riccardo.pirani@reiser.it>
2025-09-25 16:34:51 +0200

    FIx code

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a9cb8fe1bde0409905938ceb2a908fce",
+const RESOURCES = {"flutter_bootstrap.js": "772d91a1bb385e9e1a9a8766ec94d456",
 "version.json": "b128f5a3aa3588b3e69cb14cb0ac52f4",
 "index.html": "caf2573a45c5d02e16e6a69959037973",
 "/": "caf2573a45c5d02e16e6a69959037973",
-"main.dart.js": "cfa15c1eb29bf4d945f1e0e3913b59b9",
+"main.dart.js": "adfff7673c7925cb139d844fe0ef3106",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "a9cb8fe1bde0409905938ceb2a908fce",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "2143ca58803132cd9baf99a63cf270b4",
-"assets/fonts/MaterialIcons-Regular.otf": "60d2b663ecf3014e58f5ce83fc2307f1",
+"assets/fonts/MaterialIcons-Regular.otf": "ee68ab8c76ee5f3197b1249183405e12",
 "canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "canvaskit/skwasm_heavy.js": "413f5b2b2d9345f37de148e2544f584f",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
